daa2a6d8bb01b7a42fe75d521fe827f379f93748
Abiraam Srinivaasan <35276593+Abiraam@users.noreply.github.com>
2022-05-30 03:15:07 -0500

    Update RegularCustomer.java

--- src/RegularCustomer.java
+++ src/RegularCustomer.java
@@ -1,7 +1,5 @@
 // Author: Abiraam Nagarajan
 // Filename: RegularCustomer.java
-// Specification: Regular Customer extending the CustomerAbs Class
-// For: CS 2365 Object Oriented Programming Section 002
 
 public class RegularCustomer extends CustomerAbs {
     public RegularCustomer(String first_name, String last_name, String customer_type, String email_address, String discount, String phone_number) {
